dc38163647baf96a3ebbf328b95e2d435fc109c5
shuirouyan <1141761757@qq.com>
2024-12-31 16:06:11 +0800

    update hook2

--- hook2.js
+++ hook2.js
@@ -27,18 +27,18 @@ function hook() {
 
         // public class CheckRoot {
         //     private static String LOG_TAG;
-        
+
         //     static {
         //         g.b.a.a.a("pns-2.13.2.1-LogOnlineStandardCuumRelease_alijtca_plus");
         //         LOG_TAG = "CheckRoot";
         //     }
-        
+
         //     private static native boolean checkDeviceDebuggable();
-        
+
         //     private static native boolean checkRootPathSU();
-        
+
         //     private static native boolean checkSuperuserApk();
-        
+
         //     public static native String isDeviceRooted();
         // }
         // LOAD:0000000000000E98		                           Java_com_mobile_auth_gatewayauth_utils_security_CheckRoot_checkDeviceDebuggable__,\
@@ -424,10 +424,75 @@ function hook5() {
     //     }
     // });
 
-    let a = Module.findExportByName('libpns-2.13.2.1-LogOnlineStandardCuumRelease_alijtca_plus.so')
+    // let a = Module.findExportByName('libpns-2.13.2.1-LogOnlineStandardCuumRelease_alijtca_plus.so')
+    let a = Module.getBaseAddress('libc.so')
+    console.log(`${a}`)
+    let resu = a.add(0x7ffff5762620)
+    let alog = new NativeFunction(resu, null)
+    console.log(`aloglog:${alog}`)
+
+    var module = Process.getModuleByName('haha.so');
+
+    // so中的函数的地址，我这里是按64位直接写了
+    var funcPtr = module.base.add(0x185cc);
+    console.log("Function address: " + funcPtr);
+
+    var func = new NativeFunction(funcPtr, 'pointer', ['int', 'pointer'], {
+        exceptions: 'propagate'
+    });
+
+    // 字符串数组
+    var stringArray = ['asdkj12lk31', 'asdjlajdlajksj', 'sajfljkflka231'];
+    var arrayPtr = Memory.alloc(Process.pointerSize * stringArray.length);
+    for (var i = 0; i < stringArray.length; i++) {
+        var strPtr = Memory.allocUtf8String(stringArray[i]);
+        Memory.writePointer(arrayPtr.add(Process.pointerSize * i), strPtr);
+    }
+
+    // 创建参数int 和那个array
+    var arg1 = 123;
+    var arg2 = arrayPtr;
+
+    var result = func(arg1, arg2);
+
+    console.log("Function result: " + result);
+
 
 
 }
+
+// 正常写法 
+//  1、so中的函数参数是？private static native Object[] main(int i, Object[] objArr);这个看起来是定义在Java文件中的native函数声明，你确定是需要调用这个函数？
+function ahooktest() {
+
+    var module = Process.getModuleByName('haha.so');
+
+    // so中的函数的地址，我这里是按64位直接写了
+    var funcPtr = module.base.add(0x185cc);
+    console.log("Function address: " + funcPtr);
+
+    var func = new NativeFunction(funcPtr, 'pointer', ['int', 'pointer'], {
+        exceptions: 'propagate'
+    });
+
+    // 字符串数组
+    var stringArray = ['asdkj12lk31', 'asdjlajdlajksj', 'sajfljkflka231'];
+    var arrayPtr = Memory.alloc(Process.pointerSize * stringArray.length);
+    for (var i = 0; i < stringArray.length; i++) {
+        var strPtr = Memory.allocUtf8String(stringArray[i]);
+        Memory.writePointer(arrayPtr.add(Process.pointerSize * i), strPtr);
+    }
+
+    // 创建参数int 和那个array
+    var arg1 = 123;
+    var arg2 = arrayPtr;
+
+    var result = func(arg1, arg2);
+
+    console.log("Function result: " + result);
+
+}
+
 function main() {
     Java.perform(function () {
         hook5();
