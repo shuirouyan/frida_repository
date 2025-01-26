155bdbb9f83e8c0c345c7303fd5081deed1875ff
shuirouyan <1141761757@qq.com>
2025-01-26 16:23:20 +0800

    hook13

--- hook13.js
+++ hook13.js
@@ -6,17 +6,27 @@ function method01() {
     let y = Java.use("com.bytedance.sdk.account.l.b.y");
     y["a"].overload('com.bytedance.sdk.account.l.a.w').implementation = function (wVar) {
 
-        console.log('a is called' + ', ' + ' wVar: ' + wVar.f45865a);
+        console.log('a is called' + ', ' + ' wVar: ' + wVar);
         let ret = this.a(wVar);
         let alog = json_obj.$new(ret)
         console.log('a ret value is ' + alog.toString());
         return ret;
     };
+
+
     let w = Java.use("com.bytedance.sdk.account.l.a.w");
     w["$init"].overload('java.lang.String', 'java.lang.String', 'int').implementation = function (str, str2, i2) {
         console.log('$init is called' + ', ' + 'str: ' + str + ', ' + 'str2: ' + str2 + ', ' + 'i2: ' + i2);
         let ret = this.$new(str, str2, i2);
         console.log('$init ret value is ' + ret);
+        return ret;
+    };
+
+    let StringUtils = Java.use("com.bytedance.common.utility.StringUtils");
+    StringUtils["encryptWithXor"].implementation = function (str) {
+        console.log('encryptWithXor is called' + ', ' + 'str: ' + str);
+        let ret = this.encryptWithXor(str);
+        console.log('encryptWithXor ret value is ' + ret);
         return ret;
     };
 }
