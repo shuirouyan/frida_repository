b565f0f36ffe49197b26027ab163ab5aec2f351a
shuirouyan <1141761757@qq.com>
2025-01-26 16:16:18 +0800

    hook13

--- hook13.js
+++ hook13.js
@@ -5,10 +5,11 @@ function method01() {
     let json_obj = Java.use("org.json.JSONObject");
     let y = Java.use("com.bytedance.sdk.account.l.b.y");
     y["a"].overload('com.bytedance.sdk.account.l.a.w').implementation = function (wVar) {
-        let alog = json_obj.$new(wVar)
-        console.log('a is called' + ', ' + 'wVar: ' + alog.toString());
+
+        console.log('a is called' + ', ' + ' wVar: ' + wVar);
         let ret = this.a(wVar);
-        console.log('a ret value is ' + ret);
+        let alog = json_obj.$new(ret)
+        console.log('a ret value is ' + alog.toString());
         return ret;
     };
 
