ad2b4fb0f044090a1835debf0ff3202de2b89809
shuirouyan <1141761757@qq.com>
2025-01-31 17:15:22 +0800

    hook15

--- hook15.js
+++ hook15.js
@@ -2,12 +2,12 @@ function method01() {
     let process_id = Process.id
     console.log(`process id:${process_id}`)
     let Intrinsics = Java.use("kotlin.jvm.internal.Intrinsics");
-    Intrinsics["checkNotNullExpressionValue"].implementation = function (obj, str) {
-        console.log('checkNotNullExpressionValue is called' + ', ' + 'obj: ' + obj + ', ' + 'str: ' + str);
-        let ret = this.checkNotNullExpressionValue(obj, str);
-        console.log('checkNotNullExpressionValue ret value is ' + ret);
-        return ret;
-    };
+    // Intrinsics["checkNotNullExpressionValue"].implementation = function (obj, str) {
+    //     console.log('checkNotNullExpressionValue is called' + ', ' + 'obj: ' + obj + ', ' + 'str: ' + str);
+    //     let ret = this.checkNotNullExpressionValue(obj, str);
+    //     console.log('checkNotNullExpressionValue ret value is ' + ret);
+    //     return ret;
+    // };
 
 
 
