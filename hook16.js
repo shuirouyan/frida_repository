effec06d06afa4af0b438fd16633e25be06b1244
shuirouyan <1141761757@qq.com>
2025-02-15 16:54:14 +0800

    hook16

--- hook16.js
+++ hook16.js
@@ -21,86 +21,89 @@ function method01() {
         onMatch: function (instance) {
             /* 找到实例时的操作 */
             console.log(`instance:${instance}`)
-            let re = instance.db('cYoiUd2BfEDc/V9e4LdciBz9Mzwzs3yr0kgrLA==')
-            console.log(`==========>result:${re}`)
+            // cYoiUd2BfEDc/V9e4LdciBz9Mzwzs3yr0kgrLA==
+            // 2hyWtSLN69+QWLHQ
+            let res = instance.db('2hyWtSLN69+QWLHQ')
+            let re = instance.db('hjyaQ8jNSdp+mZic7Kdtyw==')
+            console.log(`==========>result:${res}${re}`)
         },
         onComplete: function () {
             /* 完成时的操作 */
             console.log(`onComplete:${new Date()}`)
         }
     })
-    Java.choose('com.zj.wuaipojie2025.T', {
-        onMatch: function (instance) {
-            /* 找到实例时的操作 */
-            console.log(`instance:${instance}`)
-            const str = "my-xxtea-secret";
-            const length = Memory.alloc(Process.pointerSize);
-            const buffer = Memory.allocUtf8String(str);
-            Memory.writeUInt(ptr(length), buffer.readUtf8String().length);
+    // Java.choose('com.zj.wuaipojie2025.T', {
+    //     onMatch: function (instance) {
+    //         /* 找到实例时的操作 */
+    //         console.log(`instance:${instance}`)
+    //         const str = "my-xxtea-secret";
+    //         const length = Memory.alloc(Process.pointerSize);
+    //         const buffer = Memory.allocUtf8String(str);
+    //         Memory.writeUInt(ptr(length), buffer.readUtf8String().length);
 
-            const byteArray = [];
-            for (let i = 0; i < Memory.readUInt(length); i++) {
-                byteArray.push(buffer.add(i).readU8());
-            }
+    //         const byteArray = [];
+    //         for (let i = 0; i < Memory.readUInt(length); i++) {
+    //             byteArray.push(buffer.add(i).readU8());
+    //         }
 
-            const uint8Array = new Uint8Array(byteArray);
-
-
-            // Base64 字符表
-            const base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-            // 自定义 Base64 解码函数
-            function base64Decode(base64Str) {
-                const output = [];
-                let input = base64Str.replace(/=+$/, '');
-                for (let i = 0; i < input.length; i += 4) {
-                    const enc1 = base64Chars.indexOf(input[i]);
-                    const enc2 = base64Chars.indexOf(input[i + 1]);
-                    const enc3 = base64Chars.indexOf(input[i + 2]);
-                    const enc4 = base64Chars.indexOf(input[i + 3]);
-
-                    const chr1 = (enc1 << 2) | (enc2 >> 4);
-                    const chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-                    const chr3 = ((enc3 & 3) << 6) | enc4;
-
-                    output.push(chr1);
-                    if (enc3 !== -1) {
-                        output.push(chr2);
-                    }
-                    if (enc4 !== -1) {
-                        output.push(chr3);
-                    }
-                }
-                return new Uint8Array(output);
-            }
-
-            // 要解码的 Base64 字符串
-            const base64Str = "cYoiUd2BfEDc/V9e4LdciBz9Mzwzs3yr0kgrLA==";
-
-            // 进行解码
-            const decodedByteArray = base64Decode(base64Str);
-
-            // 打印解码后的字节数组
-            console.log("解码后的字节数组:");
-            for (let i = 0; i < decodedByteArray.length; i++) {
-                console.log(`索引 ${i}: ${decodedByteArray[i]}`);
-            }
-
-            // 打印字节数组
-            console.log("转换后的字节数组:");
-            for (let i = 0; i < uint8Array.length; i++) {
-                console.log(`索引 ${i}: ${uint8Array[i]}`);
-            }
-            let re = instance.de(decodedByteArray, uint8Array)
-            console.log(`==========>result:${re}`)
+    //         const uint8Array = new Uint8Array(byteArray);
 
 
-        },
-        onComplete: function () {
-            /* 完成时的操作 */
-            console.log(`onComplete:${new Date()}`)
-        }
-    })
+    //         // Base64 字符表
+    //         const base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    //         // 自定义 Base64 解码函数
+    //         function base64Decode(base64Str) {
+    //             const output = [];
+    //             let input = base64Str.replace(/=+$/, '');
+    //             for (let i = 0; i < input.length; i += 4) {
+    //                 const enc1 = base64Chars.indexOf(input[i]);
+    //                 const enc2 = base64Chars.indexOf(input[i + 1]);
+    //                 const enc3 = base64Chars.indexOf(input[i + 2]);
+    //                 const enc4 = base64Chars.indexOf(input[i + 3]);
+
+    //                 const chr1 = (enc1 << 2) | (enc2 >> 4);
+    //                 const chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+    //                 const chr3 = ((enc3 & 3) << 6) | enc4;
+
+    //                 output.push(chr1);
+    //                 if (enc3 !== -1) {
+    //                     output.push(chr2);
+    //                 }
+    //                 if (enc4 !== -1) {
+    //                     output.push(chr3);
+    //                 }
+    //             }
+    //             return new Uint8Array(output);
+    //         }
+
+    //         // 要解码的 Base64 字符串
+    //         const base64Str = "cYoiUd2BfEDc/V9e4LdciBz9Mzwzs3yr0kgrLA==";
+
+    //         // 进行解码
+    //         const decodedByteArray = base64Decode(base64Str);
+
+    //         // 打印解码后的字节数组
+    //         console.log("解码后的字节数组:");
+    //         for (let i = 0; i < decodedByteArray.length; i++) {
+    //             console.log(`索引 ${i}: ${decodedByteArray[i]}`);
+    //         }
+
+    //         // 打印字节数组
+    //         console.log("转换后的字节数组:");
+    //         for (let i = 0; i < uint8Array.length; i++) {
+    //             console.log(`索引 ${i}: ${uint8Array[i]}`);
+    //         }
+    //         let re = instance.de(decodedByteArray, uint8Array)
+    //         console.log(`==========>result:${re}`)
+
+
+    //     },
+    //     onComplete: function () {
+    //         /* 完成时的操作 */
+    //         console.log(`onComplete:${new Date()}`)
+    //     }
+    // })
 }
 
 
