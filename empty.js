adabcb042d0dac023a21af7e0ff45a4fccf46ae5
shuirouyan <1141761757@qq.com>
2025-02-24 20:52:34 +0800

    empty add hook kill

--- empty.js
+++ empty.js
@@ -1,14 +1,27 @@
 function method01() {
-	let process_id=Process.id
-	console.log(`process id:${process_id}`)
-	
+    let process_id = Process.id
+    console.log(`process id:${process_id}`)
+
 }
 
+function hook_kill() {
+    Interceptor.attach(Module.findExportByName(null, 'kill'), {
+        onEnter: function (args) {
+            var pid = args[0].toInt32();  // 目标进程 ID
+            var sig = args[1].toInt32();  // 信号类型
+            console.log('kill() called with pid: ' + pid + ' and signal: ' + sig);
+        },
+        onLeave: function (retval) {
+            console.log('kill() returned');
+        }
+    });
 
+}
 function main() {
 
     Java.perform(function () {
         method01();
+        // hook_kill();
     })
 
 }
